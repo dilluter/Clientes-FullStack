913ca70647545257ea93da25cec449966095757a
igor bruno <igordf2502@gmail.com>
2025-08-26 21:48:06 -0300

    Criando repositorios JPA

--- src/main/java/io/github/dilluter/model/repository/ServicoRepository.java
+++ src/main/java/io/github/dilluter/model/repository/ServicoRepository.java
@@ -1,6 +1,8 @@
 package io.github.dilluter.model.repository;
 
 import io.github.dilluter.model.entity.Cliente;
+import io.github.dilluter.model.entity.Servico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClienteRepository extends JpaRepository <Cliente, Integer>{}
+public interface ServicoRepository extends JpaRepository<Servico, Integer> {
+}
