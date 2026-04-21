f1917c4da8817df5d88e4926639cd73e918e6efa
igord <igordf2502@gmail.com>
2026-04-21 18:16:41 -0300

    layout home

--- clientes-app/src/app/app-routing.module.ts
+++ clientes-app/src/app/app-routing.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import {LoginComponent} from './login/login.component';
+import {LayoutComponent} from './layout/layout.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent }
+    ]
+  }
 ];
 
 @NgModule({
